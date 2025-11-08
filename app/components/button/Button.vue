8cb5f20609005ca05071bebcdf8a46bf366c6c81
Alexgreb <aben01@inbox.ru>
2025-11-08 16:26:43 +0400

    удалил не нужные элементы

--- app/components/button/Button.vue
+++ app/components/button/Button.vue
@@ -4,17 +4,12 @@
       type: String,
       default: ''
     },
-    onClick: {
-      type: Function,
-      default: null
-    }
   });
 </script>
 
 <template>
   <button type="button"
           :class="[props.class]"
-          @click="onClick"
           class="wrap-anywhere bg-black text-white break-all text-md px-3 p-2.5">
     <slot />
   </button>
